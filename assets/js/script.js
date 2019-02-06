1a3f060f013b2c0c4b009303f582199976416cb7
Damiën Peerbolte <99044951@mydavinci.nl>
2019-02-06 08:46:23 +0100

    Added Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
+const introScreen = document.getElementById('startPage');
+const quizScreen = document.getElementById('quizPage');
+const quizTitle = document.getElementById('questionTitle');
+const question = document.getElementById('question');
+
+var index = 0;
+
+function startPage() {
+    quizPage.style.display = "none";
+    introScreen.style.display = "block";
+}
+
 function toQuiz() {
-    document.getElementById('startPage').style.display = "none";
-    document.getElementById('quizPage').style.display = "block";
+    introScreen.style.display = "none";
+    quizPage.style.display = "block";
+    quizTitle.innerHTML = index+1 + ". " + subjects[index].title;
+    question.innerHTML = subjects[index].statement;
+}
+
+function nextQuestion() {
+    if(index == 6) {
+        console.log("Done");
+    } else {
+        index++;
+        quizTitle.innerHTML = index+1 + ". " + subjects[index].title;
+        question.innerHTML = subjects[index].statement;
+    }
 }
